4265bfc295c5742195d030f64c73dcb8881838af
tytwitchell <tytwitchell@gmail.com>
2023-08-25 15:19:54 -0400

    select box on change event
    adding change event to selectbox

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const colorInput = document.getElementById("color-input")
+const colorSelect = document.getElementById("color-select")
 const getSchemeBtn = document.getElementById("get-scheme-btn")
 const schemeContainer = document.getElementById("scheme-container")
-
 let fetchedData = ''
 
-colorInput.addEventListener('input', function(){
+colorInput.addEventListener('input', () => fetchApi())
+colorSelect.addEventListener('change', () => fetchApi())
+
+function fetchApi(){
     const inputValue = colorInput.value.replace('#', '')
-    const colorSelectValue = document.getElementById("color-select").value
+    const colorSelectValue = colorSelect.value
 
     fetch(`https://www.thecolorapi.com/scheme?hex=${inputValue}&mode=${colorSelectValue}`)  
     .then(res => res.json())
     .then(data => fetchedData = data)
-
-})
+}
 
 
 getSchemeBtn.addEventListener('click', function(){
@@ -25,13 +27,10 @@ getSchemeBtn.addEventListener('click', function(){
             <div class="color" style="background-color: ${color.hex.value};" id="${color.hex.value}"></div>
             <p>${color.hex.value}</p>
         </div>`).join('')
-    // ADD HTML HERE using fetchedData
+
     schemeContainer.innerHTML = colorHtml
 })
 
-document.getElementsByClassName("color").addEventListener('click', function(){
-
-})
 
 
 
